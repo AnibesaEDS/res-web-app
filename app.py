6479e1cc03f9598a79edbbd6a2a2dcf5500fca9d
mando <ethandsmith15@gmail.com>
2021-10-13 09:33:46 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+from PIL import Image
+
+st.title('RES Data Visualization Tool')
+
+date = st.date_input("Select a date", datetime.date(2021, 9, 15))
+
+options = st.multiselect('Select what plots to show',
+                         [
+                             'Timestamp_vs_Ambient_Temp',
+                             'Timestamp_vs_Irradiance',
+                             'Timestamp_vs_panel_output_watt_hours_X',
+                             'Timestamp_vs_Panel_X_Watts',
+                             'Timestamp_vs_Temp_X',
+                             'Timestamp_vs_Temp_X_and_Ambient_Temp',
+                         ],
+                         ['Timestamp_vs_Irradiance', 'Timestamp_vs_Panel_X_Watts', 'Timestamp_vs_Temp_X_and_Ambient_Temp'])
+
+
+##
+#
+for x in options:
+    image = Image.open(
+        '../processed_data/' + str(date) + '/' + str(x) + '.png')
+    st.image(image, caption=str(x), width=None)
+
+
+##
+#
+f = open("../processed_data/"+ str(date) + "/daily_summmary.txt", "r")
+daily_summary_array = f.read().split('\n')
+#
+st.write('\n----------  Daily Summary   ----------\n')
+#
+st.write(str(daily_summary_array[0]))
+st.write(str(daily_summary_array[1]))
+st.write(str(daily_summary_array[2]))
+st.write(str(daily_summary_array[3]))
+#
+st.write('\n')
+#
+st.write(str(daily_summary_array[5]))
+st.write(str(daily_summary_array[6]))
+st.write(str(daily_summary_array[7]))
+st.write(str(daily_summary_array[8]))
+#
+st.write('\n----------  Daily Summary   ----------\n')
