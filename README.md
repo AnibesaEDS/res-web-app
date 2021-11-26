c9532aec9abc5f38bee237de184a6923696d68a8
mando <ethandsmith15@gmail.com>
2021-11-26 12:01:52 -0400

    added data and changed some code
    - added new processed data
    
    - changed how existing and new graphs are plotted. Added dots for each data point
    
    - added normalized data
    
    - changed web app to accommodate new chart names

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-https://share.streamlit.io/anibesaeds/res-web-app/main/web_app/app.py
+web app link - https://share.streamlit.io/anibesaeds/res-web-app/main/web_app/app.py
+
+Missing data :
+2021-10-27 - 2021-11-01
--- web_app/app.py
+++ web_app/app.py
@@ -11,47 +11,20 @@ date = st.date_input("Select a date", datetime.date(2021, 9, 15))
 
 options = st.multiselect('Select what plots to show',
                          [
-                             'Timestamp_vs_Ambient_Temp',
-                             'Timestamp_vs_Irradiance',
-                             'Timestamp_vs_panel_output_watt_hours_X',
-                             'Timestamp_vs_panel_output_watt_hours_normalized_X',
-                             'Timestamp_vs_Panel_X_Watts',
-                             'Timestamp_vs_Panel_X_Watts_Normalized',
-                             'Timestamp_vs_Temp_X',
-                             'Timestamp_vs_Temp_X_and_Ambient_Temp',
+                             'sorted_timestamp_vs_temp_x_and_ambient_temp',
+                             'sorted_timestamp_vs_irradiance',
+                             'sorted_timestamp_vs_panel_x_watts',
+                             'sorted_normalized_panel_x_watts',
+                             'sorted_panel_x_output_watt_hours',
+                             'sorted_panel_x_theoretical_max_output_power',
                          ],
-                         ['Timestamp_vs_Irradiance', 'Timestamp_vs_Panel_X_Watts', 'Timestamp_vs_Temp_X_and_Ambient_Temp'])
-
+                         ['sorted_timestamp_vs_irradiance', 'sorted_timestamp_vs_panel_x_watts', 'sorted_timestamp_vs_temp_x_and_ambient_temp'])
 
 ##
+
 #
 for x in options:
     file_path = Path(__file__).parents[1] / \
         ('processed_data/' + str(date) + '/' + str(x))
     image = Image.open(str(file_path) + '.png')
     st.image(image, caption=str(x), width=None)
-
-
-# ##
-# #
-# file_path = Path(__file__).parents[1] / \
-#     ('processed_data/' + str(date) + '/daily_summmary.txt')
-
-# f = open(str(file_path), "r")
-# daily_summary_array = f.read().split('\n')
-# #
-# st.write('\n----------  Daily Summary   ----------\n')
-# #
-# st.write(str(daily_summary_array[0]))
-# st.write(str(daily_summary_array[1]))
-# st.write(str(daily_summary_array[2]))
-# st.write(str(daily_summary_array[3]))
-# #
-# st.write('\n')
-# #
-# st.write(str(daily_summary_array[5]))
-# st.write(str(daily_summary_array[6]))
-# st.write(str(daily_summary_array[7]))
-# st.write(str(daily_summary_array[8]))
-# #
-# st.write('\n----------  Daily Summary   ----------\n')
